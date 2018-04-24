870e79b73a69185df02ee016173e812dfd13deeb
Maxime Killinger <max.killinger@outlook.fr>
2018-04-24 14:15:32 +0200

    UPD - Translation
    Add a fully translated version.
    Change one message in french in code.
    
    Signed-off-by: Maxime Killinger <max.killinger@outlook.fr>
    
    modified:   ../../easytransac_woocommerce.php
    modified:   easytransac_woocommerce-fr_FR.mo
    modified:   easytransac_woocommerce-fr_FR.po
    deleted:    easytransac_woocommerce-fr_FR.pot

--- easytransac_woocommerce.php
+++ easytransac_woocommerce.php
@@ -242,9 +242,9 @@ function init_easytransac_gateway() {
 			} else {
 				// Phone number traitement of '+'
 				if (!preg_match("/^[0-9]{7,15}$/", $address['phone'])) {
-					$address['phone'] = str_replace("+", "00", $address['phone']);                                                                                                              
+					$address['phone'] = str_replace("+", "00", $address['phone']);
 					if (!preg_match("/^[0-9]{7,15}$/", $address['phone'])) {
-						return wc_add_notice(__('Téléphone  de facturation n’est pas un numéro de téléphone valide.', 'easytransac_woocommerce'), 'error');                                 
+						return wc_add_notice(__('Billing phone is not valid phone number.', 'easytransac_woocommerce'), 'error');
 					}
 				}
 
@@ -530,10 +530,10 @@ function init_easytransac_gateway() {
 			}
 			$refund = (new \EasyTransac\Entities\Refund)
 				->setTid(get_post_meta($order_id, 'ET_Tid', true));
-			
+
 			$request = (new EasyTransac\Requests\PaymentRefund);
 			$response = $request->execute($refund);
-			
+
 			if (empty($response)) {
 				return new WP_Error('easytransac-refunds', __('Empty Response', 'easytransac_woocommerce'));
 			}
